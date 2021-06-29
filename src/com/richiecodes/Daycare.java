1c13711f63a009ccb7fa26b36970b40f16b68f9b
Richard DeLuca <richard_deluca@outlook.com>
2021-06-29 19:23:08 -0400

    Changed for-in to for-each loop in removeAnimal method in Daycare class

--- src/com/richiecodes/Daycare.java
+++ src/com/richiecodes/Daycare.java
@@ -10,15 +10,15 @@ public class Daycare {
 
     public static void displayAnimals(List<Animal> animalList) {
         for(var a : animalList) {
-            System.out.println(a + a.speak() + "\n");
+            System.out.println(a + a.speak() + "\n-----------------------------------------");
         }
     }
 
     public static void removeAnimal(String name, List<Animal> animalList) {
-        for(var a : animalList) {
-            if(a.getName().equalsIgnoreCase(name)) {
-                System.out.println("Removed \"" + a.getName() + "\" from the Daycare\n");
-                animalList.remove(a);
+        for(int i = 0; i < animalList.size(); i++) {
+            if(animalList.get(i).getName().equalsIgnoreCase(name)) {
+                System.out.println("Removed \"" + animalList.get(i).getName() + "\" from the Daycare\n");
+                animalList.remove(animalList.get(i));
             } else {
                 throw new IllegalArgumentException("Error: Animal name \"" + name + "\" cannot be" +
                         " found in list");
